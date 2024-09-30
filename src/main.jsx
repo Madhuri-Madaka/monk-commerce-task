694850e438cbe7bd08488e6c734fa7b6a1f5f5ae
Madhuri-Madaka <madhurimadaka01@gmail.com>
2024-09-30 23:41:15 +0530

    Removed scrollbar package

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Scrollbar } from 'react-scrollbars-custom';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 import './index.css'
 import { ScopedCssBaseline, ThemeProvider } from '@mui/material';
